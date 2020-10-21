024cd638dea8b0085a7f336dc15104dec416ca2d
Zerep08 <francisco.pgarcia28@gmail.com>
2020-10-21 18:40:56 -0500

    Finish the script test_go_back_from_inventory_item_page_to_inventory_page

--- tests/inventory_item_test.py
+++ tests/inventory_item_test.py
@@ -29,3 +29,4 @@ def test_go_back_from_inventory_item_page_to_inventory_page(setup):
     inventory_page = login_page.actions.login(setup["UserName"], setup["Password"])
     inventory_item_page = inventory_page.actions.click_on_random_product()
     inventory_item_page.actions.click_on_back_button()
+    element_is_displayed(inventory_page.banner.swag_labs_label)
